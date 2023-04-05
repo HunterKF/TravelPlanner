39f46e34c47c95a02078747c94da1f031bb0bbfa
HunterKF <95839328+HunterKF@users.noreply.github.com>
2023-04-06 06:29:34 +0900

    Single trip now works!

---
+++ app/src/main/java/com/jaegerapps/travelplanner/data/mappers/GoogleMappers.kt
@@ -0,0 +1,46 @@
+package com.jaegerapps.travelplanner.data.mappers
+
+import com.jaegerapps.travelplanner.data.models.google_places.AutoCompleteDto
+import com.jaegerapps.travelplanner.data.models.google_places.LocationResultWrapperDto
+import com.jaegerapps.travelplanner.data.models.google_places.PlaceResultDto
+import com.jaegerapps.travelplanner.domain.models.google.*
+
+fun PlaceResultDto.toPlaceWrapper(): PlaceWrapper {
+    var newPlaces = listOf<PlaceInfo>()
+    println(this.places)
+    this.places.forEach {
+        newPlaces = newPlaces.plus(
+            PlaceInfo(
+                name = it.name,
+//                type = it.types
+            )
+        )
+    }
+    println(newPlaces)
+
+
+    return PlaceWrapper(
+        nextPage = nextPage,
+        places = newPlaces
+    )
+}
+
+fun AutoCompleteDto.toGooglePrediction(): GooglePrediction {
+    var predictionsList = listOf<GooglePredictionTerm>()
+    predictions.forEach {
+        predictionsList = predictionsList.plus(
+            GooglePredictionTerm(
+                name = it.description,
+                placeId = it.placeId
+            )
+        )
+    }
+    return GooglePrediction(predictionsList)
+}
+
+fun LocationResultWrapperDto.toPlaceLocationInfo(): PlaceLocationInfo {
+    return PlaceLocationInfo(
+        lat = result.geometry.location.lat,
+        long = result.geometry.location.lng
+    )
+}
